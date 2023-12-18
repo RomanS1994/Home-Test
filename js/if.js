d2957626bbdb6d696219b1519255b99c96c82a42
Roman <140334590+RomanS1994@users.noreply.github.com>
2023-12-18 18:14:55 +0100

    update freeCodeCamp

--- js/if.js
+++ js/if.js
@@ -49,3 +49,53 @@
 // }
 
 // console.log(golfScore(4, 4));
+
+/****** Вибір із багатьох варіантів за допомогою інструкцій switch ******/
+
+// function caseInSwitch(val) {
+//   let answer = "";
+
+//   switch (val) {
+//     case 1:
+//       answer += "alpha";
+//       break;
+//     case 2:
+//       answer += "beta";
+//       break;
+//     case 3:
+//       answer += "gamma";
+//       break;
+//     case 4:
+//       answer += "delta";
+//   }
+
+//   return answer;
+// }
+
+// console.log(caseInSwitch(4));
+
+/****** Додавання опції за замовчуванням у операторах Switch ******/
+
+// function switchOfStuff(val) {
+//   let answer = "";
+//   // Only change code below this line
+//   switch (val) {
+//     case "a":
+//       answer += "apple";
+//       break;
+//     case "b":
+//       answer += "bird";
+//       break;
+//     case "c":
+//       answer += "cat";
+//       break;
+//     default:
+//       answer += "stuff";
+//       break;
+//   }
+
+//   // Only change code above this line
+//   return answer;
+// }
+
+// console.log(switchOfStuff("b"));
