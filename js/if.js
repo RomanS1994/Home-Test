2f1c058b96c1b0f64e8b48858030fa8dd5bf3cc3
Roman <140334590+RomanS1994@users.noreply.github.com>
2023-12-19 22:49:39 +0100

    update js

--- js/if.js
+++ js/if.js
@@ -187,3 +187,43 @@
 // console.log(cardCount(7));
 // console.log(cardCount("K"));
 // console.log(cardCount(4));
+
+// Налаштування
+// function phoneticLookup(val) {
+//   let result = "";
+
+//   // Змініть код лише під цим рядком
+//   const lookup = {
+//     alpha: "Adams",
+//     bravo: "Boston",
+//     charlie: "Chicago",
+//     delta: "Denver",
+//     echo: "Easy",
+//     foxtrot: "Frank",
+//   };
+
+//   // Змініть код лише над цим рядком
+//   return lookup[val];
+// }
+
+// phoneticLookup("charlie");
+
+// Налаштування
+function phoneticLookup(val) {
+  let result = "";
+
+  // Змініть код лише під цим рядком
+  const lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank",
+  };
+  result = lookup["val"];
+  // Змініть код лише над цим рядком
+  return result;
+}
+
+phoneticLookup("charlie");
