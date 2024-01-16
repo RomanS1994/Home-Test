b9f10a10d54cf11d4a22ba2221614da28935ce64
Roman <140334590+RomanS1994@users.noreply.github.com>
2024-01-16 14:59:28 +0100

    add lessons

---
+++ lessons/module8__1-2/mod8.js
@@ -0,0 +1,50 @@
+/**
+  |============================
+  | Lesson 15
+  |============================
+*/
+/* --- ініціалізація НОДИ команда  npm init 
+---   Одноразово
+---   Встановлює файл package.json - для встановлення бібліотек.
+*/
+
+/****** Встановлення Babel ******/
+/* для кросбраузерності (нові функції в старі)*/
+/*
+ *
+ *
+ *
+ */
+/**
+  |============================
+  | lesson 16
+  |============================
+*/ /****** Робота з локал сторедж ******/
+// const LS_KEY = "Array of names";
+// const names = ["Allice", "Kate", "Emma"];
+// /****** Збереження в LocalStoreg ******/
+
+// localStorage.setItem(LS_KEY, JSON.stringify(names));
+
+/****** Читання з Ls ******/
+// const value = localStorage.getItem(LS_KEY);
+// console.log(value);
+// console.log(JSON.parse(value));
+
+/****** Видалення з лс ******/
+// localStorage.removeItem(LS_KEY);
+
+/******  Чому варто завжди використовувати json.stringify ******/
+// const LS_KEY = "Array of names";
+// const names = ["Allice", "Kate", "Emma"];
+
+// localStorage.setItem(LS_KEY, names);
+// const value = localStorage.getItem(LS_KEY);
+// console.log(value);
+// console.log(JSON.parse(value));
+
+/****** try catch ******/
+// try {
+// } catch (error) {}
+
+/*********** Практика ************/
