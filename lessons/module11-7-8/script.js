16427269a432943d9ff1979e8555077bedb88f7f
Roman <140334590+RomanS1994@users.noreply.github.com>
2024-01-31 01:04:26 +0100

    add server

---
+++ lessons/module11-7-8/script.js
@@ -0,0 +1,21 @@
+//******************** CRUD ******************** \\
+
+// Create - POST
+// Read   - GET
+// Update - PUT PATCH
+// DELETE - DELETE
+
+const url = "https://jsonplaceholder.typicode.com/posts/3";
+
+// ******************** GET - READ ******************** \\
+
+// fetch(url)
+//   .then((r) => r.json())
+//   .then((json) => console.log(json));
+
+// ******************** POST - CREATE ******************** \\
+
+const post = {
+  title: "Group 2",
+  bodi,
+};
