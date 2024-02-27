794c303fa6955956c6ac57bbc25ab0ea367dddcf
Roman <140334590+RomanS1994@users.noreply.github.com>
2024-02-27 20:03:15 +0100

    add new task

---
+++ codewars/kyu8/findtheSmallestIntegerIntheArray.js
@@ -0,0 +1,8 @@
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+
+const result = new SmallestIntegerFinder();
+console.log(result.findSmallestInt([78, 56, 232, 12, 8]));
