2c491bcf080da8c73b83f4a6869b9576ddba247b
Roman <140334590+RomanS1994@users.noreply.github.com>
2024-02-29 21:22:15 +0100

    ...

--- codewars/kyu8/getCharacterFromASCIIValue.js
+++ codewars/kyu8/getCharacterFromASCIIValue.js
@@ -6,6 +6,6 @@ function asciiSymbol(number) {
 // Приклад використання
 const number = 61;
 
-console.log(asciiSymbol(number));
-console.log("Символ ASCII для числа", number, ":", asciiSymbol(number));
-console.log(String.fromCharCode(189, 43, 190, 61));
+// console.log(asciiSymbol(number));
+// console.log("Символ ASCII для числа", number, ":", asciiSymbol(number));
+// console.log(String.fromCharCode(189, 43, 190, 61));
