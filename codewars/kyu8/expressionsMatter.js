7180405fffc75fd0a560b33bc4ebb3f43b40466b
Roman <140334590+RomanS1994@users.noreply.github.com>
2024-03-04 22:08:16 +0100

    new function

--- codewars/kyu8/expressionsMatter.js
+++ codewars/kyu8/expressionsMatter.js
@@ -1,3 +1,22 @@
+// function basicOp(operation, value1, value2) {
+//   switch (true) {
+//     case operation === "-":
+//       return value1 - value2;
+//       break;
+//     case operation === "+":
+//       return value1 + value2;
+//       break;
+//     case operation === "/":
+//       return value1 / value2;
+//       break;
+//     case operation === "*":
+//       return value1 * value2;
+//       break;
+//   }
+//   return eval(value1 + operation + value2);
+// }
+// console.log(basicOp("*", 15, 18));
+
 function expressionMatter(a, b, c) {
   const var1 = a * (b + c);
   const var2 = a * b * c;
@@ -7,4 +26,4 @@ function expressionMatter(a, b, c) {
 
   return Math.max(var1, var2, var3, var4, var5);
 }
-console.log(expressionMatter(1, 3, 1));
+// console.log(expressionMatter(1, 3, 1));
